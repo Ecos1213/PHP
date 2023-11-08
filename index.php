55cf8f21d1921c2d6a1acac47b744165e07d6689
Ecos1213 <diasflac1213@gmail.com>
2023-11-08 11:43:20 -0500

    Agrega explicacion codigo de debuggin y comentarios

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <?php
 
+$personas = [
+    "Carlos" => 22,
+    "Mr. michi" => 15,
+    "Juan" => 65
+];
 
-echo "hola, mama. estoy programando con PHPCito \n";
 
-$nombre = "Carlos";
+/*La función var_dump() en PHP se utiliza para mostrar información sobre una o más variables, incluyendo su tipo de dato y su contenido. 
+Su principal propósito es la depuración y el diagnóstico de variables y estructuras de datos en tus programas PHP */
 
-$apellido = "Santana";
+var_dump($personas);
+echo "\n";
 
-echo "No, yo me llamo " . $nombre . " " . $apellido . "\n";
+/*La función print_r() en PHP se utiliza para mostrar información sobre una variable o estructura de datos de una manera más legible que var_dump(). 
+A diferencia de var_dump(), print_r() no muestra el tipo de dato ni la longitud de las cadenas, lo que la hace más adecuada para la visualización de 
+datos en un formato más humano. */
+print_r($personas);
+echo "\n";
 
-// forma mas sencilla de imprimir
-echo "No, no. Yo me llamo $nombre $apellido \n"; // php es capaz de leer variables en cualquier comillas dobles 
+//esto es un comentario
 
-echo "El resultado de 4 x 5 es " . (4 * 5) . "\n"; 
+#esto es tambien un comentario
+
+/*tambie es un comentario 
+de multiples lineas */
+
+
 
