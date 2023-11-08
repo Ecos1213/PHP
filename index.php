df4e539d690fe594f2291bcbe0da0ca56e470494
Ecos1213 <diasflac1213@gmail.com>
2023-11-08 08:36:22 -0500

    cambia a desmotracion de sintasix de php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "hola, mundo";
+
+echo "hola, mama. estoy programando con PHPCito \n";
+
+$nombre = "Carlos";
+
+$apellido = "Santana";
+
+echo "No, yo me llamo " . $nombre . " " . $apellido . "\n";
+
+// forma mas sencilla de imprimir
+echo "No, no. Yo me llamo $nombre $apellido \n"; // php es capaz de leer variables en cualquier comillas dobles 
+
+echo "El resultado de 4 x 5 es " . (4 * 5) . "\n"; 
+
