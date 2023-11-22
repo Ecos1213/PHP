286122c402909cf69accc28f439099496d9ea4ca
Ecos1213 <diasflac1213@gmail.com>
2023-11-22 16:14:33 -0500

    Agrega clase de arreglos

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 
+$edades = [20, 18, 40];
 
-$horas = readline("Ingresa Horas: "); 
+// otra forma de declarar un arreglo es usando la funcion array 
+$edades2 = array(20, 18, 40);
 
-$minutos = readline("Ingresa Minutos: "); 
-
-$segundos = readline("Ingresa segundos: "); 
-
-$segundos += $horas * 3600;
-
-$segundos += $minutos * 60;
-
-echo "el tiempo asignado en segundos es de $segundos";
+echo "Una de las edades que esta dentro es " .  $edades[1] . " ";
+echo "Una de las edades que esta dentro es " .  $edades[0] . " ";
+echo "Una de las edades que esta dentro es " .  $edades[2] . " ";
 
 echo "\n";
+
+
+echo "Una de las edades que esta dentro es " .  $edades2[1] . " ";
+echo "Una de las edades que esta dentro es " .  $edades2[0] . " ";
+echo "Una de las edades que esta dentro es " .  $edades2[2] . " ";
