166a56f09b92871c464877914483e769c8d2e50d
Ecos1213 <diasflac1213@gmail.com>
2023-11-29 11:42:12 -0500

    Agrega reto de los michis con la llave ${}

--- index.php
+++ index.php
@@ -1,32 +1,58 @@
 <?php
-// aca estan todas las funciones que maneja un array https://www.php.net/manual/es/ref.array.php ya que solo veremos algunas 
-$edades = [18, 22, 40, 34];
 
-// count
-echo count($edades) . "\n"; // cuenta cuantos elementos hay un arreglo, lleva como parametro el array
+$michis = array(
+    array(
+        "nombre" => "Daniel",
+        "ocupacion" => "ingeniero de sistemas",
+        "color" => "purpura",
+        "comidas" => array(
+            "comidasFavoritas" => "tamal, pollo al horno, pizza",
+            "comidasNoFavoritas" => "ullucos, zapallo",
+        ),
+    ),
+    array(
+        "nombre" => "Miguel",
+        "ocupacion" => "ingeniero de sistemas",
+        "color" => "azul",
+        "comidas" => array(
+            "comidasFavoritas" => "chuleta de pollo",
+            "comidasNoFavoritas" => "ullucos",
+        ),
+    ),
+    array(
+        "nombre" => "Daniel",
+        "ocupacion" => "Diseñador Grafico",
+        "color" => "purpura",
+        "comidas" => array(
+            "comidasFavoritas" =>  "chuleta de pollo, hamburguesa",
+            "comidasNoFavoritas" => "zapallo",
+        ),
+    ),
+    array(
+        "nombre" => "Alvaro",
+        "ocupacion" => "ingeniero electronico",
+        "color" => "amarillo",
+        "comidas" => array(
+            "comidasFavoritas" => "pescado frito",
+            "comidasNoFavoritas" => "limon, pizza",
+        ),
+    ),
+);
 
-// array_push 
-array_push($edades, 13); // agrega un valor al final del array, el primer parametro es el arreglo y el segundo es el valor que agregara al final del array
-var_dump($edades);
-echo "\n";
+for($i = 0; $i < count($michis); $i++) {
+    $michiArray = $michis[$i];
+    echo "\n";
+    echo "Nombre:  ${michiArray['nombre']}"; // recuerda esto ${michiArray['nombre']} ya esta deprecrated y ademas que solo se puede colocar un solo nivel del array si no dara error
+    echo "\n";
+    echo "Ocupacion: " . $michis[$i]["ocupacion"] . "\n";
+    echo "Color: " . $michis[$i]["color"] . "\n";
 
-// is_array
-$esto_no_es_un_arreglo = "";
+    
+    $comidasFavoritas = $michis[$i]["comidas"]["comidasFavoritas"];
+    $comidasNoFavoritas = $michis[$i]["comidas"]["comidasNoFavoritas"];
+    echo "Comidas favoritas: " . $comidasFavoritas ;
 
-var_dump(is_array($esto_no_es_un_arreglo));// define o para ver si alguna variable o cualquier cosa es un arreglo, true si es un arreglo, false si no lo es
-echo "\n";
-
-var_dump(is_array($edades));
-echo "\n";
-
-// explode
-$lista_de_frutas = "fresa,cereza,manzana";
-$lista_de_frutas_array = explode(",", $lista_de_frutas);// explode divide un string segun el argumento por cual dividiremos y este resultado lo convierte en un array, el primer argumento seria el separador el cual dividira el metodo y el segundo el string
-var_dump($lista_de_frutas_array);
-echo "\n";
-
-// implode
-$lista_de_frutas_array = ["fresa", "cereza", "manzana"];
-$lista_de_frutas = implode(",",$lista_de_frutas_array); // implode lo que hace es convertir el array en un string si queremos podemos colocar una concatenacion, el primer parametro sera la concatenacion que queramos colocarle al string y el segundo seria el array (el string quedaria asi con la concatenacion "fresa,cereza,manzana")
-var_dump($lista_de_frutas);
-echo "\n";
+    echo "\n";
+    echo "Comidas No favoritas: " . $comidasNoFavoritas;
+    echo "\n";
+}
