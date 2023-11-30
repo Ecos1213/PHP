fab8d43a622646d6f76bdbaee60874f68d9603bb
Ecos1213 <diasflac1213@gmail.com>
2023-11-30 10:16:17 -0500

    Agrega clase de if else if y else

--- index.php
+++ index.php
@@ -1,58 +1,26 @@
 <?php
 
-$michis = array(
-    array(
-        "nombre" => "Daniel",
-        "ocupacion" => "ingeniero de sistemas",
-        "color" => "purpura",
-        "comidas" => array(
-            "comidasFavoritas" => "tamal, pollo al horno, pizza",
-            "comidasNoFavoritas" => "ullucos, zapallo",
-        ),
-    ),
-    array(
-        "nombre" => "Miguel",
-        "ocupacion" => "ingeniero de sistemas",
-        "color" => "azul",
-        "comidas" => array(
-            "comidasFavoritas" => "chuleta de pollo",
-            "comidasNoFavoritas" => "ullucos",
-        ),
-    ),
-    array(
-        "nombre" => "Daniel",
-        "ocupacion" => "Diseñador Grafico",
-        "color" => "purpura",
-        "comidas" => array(
-            "comidasFavoritas" =>  "chuleta de pollo, hamburguesa",
-            "comidasNoFavoritas" => "zapallo",
-        ),
-    ),
-    array(
-        "nombre" => "Alvaro",
-        "ocupacion" => "ingeniero electronico",
-        "color" => "amarillo",
-        "comidas" => array(
-            "comidasFavoritas" => "pescado frito",
-            "comidasNoFavoritas" => "limon, pizza",
-        ),
-    ),
-);
+$asientos_disponibles = 4;
+$es_hijo_de_tom_holland = true;
 
-for($i = 0; $i < count($michis); $i++) {
-    $michiArray = $michis[$i];
-    echo "\n";
-    echo "Nombre:  ${michiArray['nombre']}"; // recuerda esto ${michiArray['nombre']} ya esta deprecrated y ademas que solo se puede colocar un solo nivel del array si no dara error
-    echo "\n";
-    echo "Ocupacion: " . $michis[$i]["ocupacion"] . "\n";
-    echo "Color: " . $michis[$i]["color"] . "\n";
-
-    
-    $comidasFavoritas = $michis[$i]["comidas"]["comidasFavoritas"];
-    $comidasNoFavoritas = $michis[$i]["comidas"]["comidasNoFavoritas"];
-    echo "Comidas favoritas: " . $comidasFavoritas ;
-
-    echo "\n";
-    echo "Comidas No favoritas: " . $comidasNoFavoritas;
-    echo "\n";
+if($asientos_disponibles > 0) {
+    echo "Puedes ver la pelicula de Spidey";
 }
+else if($es_hijo_de_tom_holland == true) {
+    echo "No te creo, pero puedes ver la pelicula";
+}
+else {
+    echo "Lo sentimos, te tocara spoilearte";
+}
+
+echo "\n";
+
+if($asientos_disponibles < 0)
+    echo "Puedes ver la pelicula de Spidey";
+else if($es_hijo_de_tom_holland) // como el if acepta solo booleanos podemos quitar el == true
+    echo "No te creo, pero puedes ver la pelicula";
+else
+    echo "Lo sentimos, te tocara spoilearte";
+#tambien podemos quitar las llaves {} del if y else solo si esta tiene una sola linea de codigo
+
+echo "\n";
