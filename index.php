24098ebb4318e919af7f933efca748f32ce523ca
Ecos1213 <diasflac1213@gmail.com>
2023-12-01 13:10:30 -0500

    Agrega reto de retirar donaciones de twitch con if else

--- index.php
+++ index.php
@@ -1,52 +1,15 @@
 <?php
-/*
-
-el problema del switch es que es polemico por que permite el codigo espagueti
-
-pero cuando debemos usar switch?
-
-usalo cuando:
-
-1. necesites decidir entre un caso u otro 
-
-2. quieras una forma fecil de plantear diversas situaciones
-
-3. usarlo no signifique sacrificar la legibilidad de tu codigo
-
-no lo uses cuando:
-
-1. necesites hacer comparaciones complejas
-
-2. quieras verificar un rango de numeros
-
-3. necesites poner grandes bloques de codigo y/o tu codigo se vuelva ilegible
-
-*/
 
 
-//michi 1: 9
-//michi 2 : 4
-//michi 3 : 3
-//michi 4: 9
-//michi 5: 1
 
-$michi = 4;
+$donations = readline("Profavor ingresa la cantidad de donaciones acumuladas a retirar: ");
 
-switch($michi) {
-    case 1:
-    case 4:
-        echo "Su numero favorito es el 9";
-        break;
-    case 2:
-        echo "Su numero favorito es el 4";
-        break;
-    case 3:
-        echo "Su numero favorito es el 3";
-        break;
-    case 5:
-        echo "Su numero favorito es el 1";
-        break;
-    default:
-        echo "Ese michi no existre :c";
+if( $donations >= 100) {
+    echo "has retirado un total de $donations";
+} else {
+    echo "No tienes fondos suficientes para poder retirarlo, tienes que tener mas de 100 para poder retirar";
 }
+
+
+
 
