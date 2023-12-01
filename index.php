1e90fd714ff0bc1b11348c30798cf1a8bc6b584c
Ecos1213 <diasflac1213@gmail.com>
2023-12-01 14:47:56 -0500

    Agrega clase de ciclo while

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 
+$contador = 0;
 
-
-$donations = readline("Profavor ingresa la cantidad de donaciones acumuladas a retirar: ");
-
-if( $donations >= 100) {
-    echo "has retirado un total de $donations";
-} else {
-    echo "No tienes fondos suficientes para poder retirarlo, tienes que tener mas de 100 para poder retirar";
+while ($contador <= 10) {
+    echo "Actualmente estamos en la iteracion $contador";
+    echo "\n";
+    $contador++;
 }
 
-
-
-
+echo "\n";
