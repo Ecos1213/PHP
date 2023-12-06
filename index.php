a2d018275ed78c0ee20cedce5c130cd1075f1d79
Ecos1213 <diasflac1213@gmail.com>
2023-12-05 19:01:24 -0500

    Agrega clase de expande arsenal de funciones predefinidas donde todo es comentarios mostrando el manual de php y algunas funciones en comentarios tanto de funciones de arreglos, tipo fecha, de strings, de funciones matematicas, etc

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <?php
 
-// el operador de nave espacial nos sirve para organizar un arreglo usando la funcion usort, esta funcion ordena un array segun sus valores usando una funcion de comparacion definida por el usuario
-// el usort realmente no devuelve un arreglo si no que modifica diractamente el arreglo original que nosotros le damos como parametro
-$precios_de_cafes = [12, 56, 32, 89, 2, 1];
+// funciones de strings https://www.php.net/manual/es/ref.strings.php
 
-usort($precios_de_cafes, function($a, $b){ // asi como en javascript php tambien nos permite crear una funcin anonima solo con la palabra function y sus parentesis ()
-    return $a <=> $b; // retornaremos la comparacion para que cree solo un array, esta comparacion tiene que retornar el valor menor, igual o mayor por eso se usa el nave espacial ya que devolvera un valor menor que es -1 asi usrot considerara que este valor es menor el primer valor que el segundo, 0 si son iguales y 1 si es mayor y asi nuestro usort ordenara nuestro array
-}); // usort como primer parametro tenemos que darle el array, este array se modificara dirctamente osea modificara el array original y no devolvera uno nuevo y como segundo parametro tenemos que colocar un callable o callback, osea una funcion que realize la comparasion del arreglo y este le colocaremos una funcion anonima, este callback recibe dos paramaetros que son los valores del array, el primer parametro usara el primer valor del array y el segundo parametro sera el segundo valor del array y estos valores seguiran tomando el tercer valor del array como primer parametro y el 4 valor del array como segundo parametro hasta terminar el array
+// un ejemplo es el metodo addslashes que escapa un string con barras invertidas como por ejemplo \n
+// otro ejemplo seria htmlspecialchars que convierte caracteres especiales en entidades html
+// otro ejemplo de ltrim que retira los espacios en blanco (u otros caracteres) del inicio de un string
+// otro ejemplo seria number_format que sirve para formatear un numero con los millares agrupados
 
 
-var_dump($precios_de_cafes);
+// funciones matematicas https://www.php.net/manual/es/ref.math.php
+
+// un ejemplo seria abs que es valor absoluto
+// otro ejemplo acos ques que saca el arco coseno
+// otro ejemplo seria floor que redondea fracciones hacia abajo
+// otro ejemplo seria is_finite que encuentra si un valor es un numero finito legal
+// otro ejemplo seria is_infinite que encuentra si un valor es un nvalor infinito 
+// otro ejemplo seria log que seria logaritmo natural
+// otro ejemplo seria max que encuentra un valor mas alto
+// otro ejemplo seria min encuentra un valor mas bajo
+// otro ejemplo seria pi que obtiene el valor de pi
+// otro ejemplo seria pow que seria expresion exponencial
+// otro ejemplo seria sqrt que seria raiz cuadrada
+
+
+// funciones para arreglos https://www.php.net/manual/es/ref.array.php
+
+//las que vimos como explode, usort, array_merge etc
+
+// funciones para fechas https://www.php.net/manual/es/ref.datetime.php
+
+// en php es increible trabajar con fechas comparado con javascript, ya que es increibledmente facil 
+// un ejemplo seria date_parse_from_format obtiene informacion de una fecha dada formateada de acuerdo al formato (zonas horarias)
+// otro ejemplo localtime que botiene una fecha y hora local
+// otro ejemplo seria microtime devuelve la fecha unix acual con microsegundos
+
+
