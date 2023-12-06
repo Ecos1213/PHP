eee55f60947e19a8f3a1fb12e4791b03822db513
Ecos1213 <diasflac1213@gmail.com>
2023-12-05 19:15:01 -0500

    Agrega reto donde obtiene la hora, minuto y segundo del tiempo actual y asigna la region de zona horario de bogota

--- index.php
+++ index.php
@@ -1,37 +1,15 @@
 <?php
 
-// funciones de strings https://www.php.net/manual/es/ref.strings.php
+function obtener_hora() {
+    // Establecer la zona horaria a la de tu región
+    date_default_timezone_set('America/Bogota'); // Reemplaza 'America/Bogota' con la zona horaria
+    
+    // Obtener la hora actual en el formato deseado
+    return date("g:i:s A", time());
+}
 
-// un ejemplo es el metodo addslashes que escapa un string con barras invertidas como por ejemplo \n
-// otro ejemplo seria htmlspecialchars que convierte caracteres especiales en entidades html
-// otro ejemplo de ltrim que retira los espacios en blanco (u otros caracteres) del inicio de un string
-// otro ejemplo seria number_format que sirve para formatear un numero con los millares agrupados
+echo "¡Hola! ¿Me podrias decir que hora es? \n";
+echo "¡Claro! Son las " . obtener_hora();
 
-
-// funciones matematicas https://www.php.net/manual/es/ref.math.php
-
-// un ejemplo seria abs que es valor absoluto
-// otro ejemplo acos ques que saca el arco coseno
-// otro ejemplo seria floor que redondea fracciones hacia abajo
-// otro ejemplo seria is_finite que encuentra si un valor es un numero finito legal
-// otro ejemplo seria is_infinite que encuentra si un valor es un nvalor infinito 
-// otro ejemplo seria log que seria logaritmo natural
-// otro ejemplo seria max que encuentra un valor mas alto
-// otro ejemplo seria min encuentra un valor mas bajo
-// otro ejemplo seria pi que obtiene el valor de pi
-// otro ejemplo seria pow que seria expresion exponencial
-// otro ejemplo seria sqrt que seria raiz cuadrada
-
-
-// funciones para arreglos https://www.php.net/manual/es/ref.array.php
-
-//las que vimos como explode, usort, array_merge etc
-
-// funciones para fechas https://www.php.net/manual/es/ref.datetime.php
-
-// en php es increible trabajar con fechas comparado con javascript, ya que es increibledmente facil 
-// un ejemplo seria date_parse_from_format obtiene informacion de una fecha dada formateada de acuerdo al formato (zonas horarias)
-// otro ejemplo localtime que botiene una fecha y hora local
-// otro ejemplo seria microtime devuelve la fecha unix acual con microsegundos
-
+echo "\n";
 
