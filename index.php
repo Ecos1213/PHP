19a68c361df8dd72dc83e2419dedefd9813346bd
Ecos1213 <diasflac1213@gmail.com>
2023-12-19 11:19:09 -0500

    Agrega operadores de estructura de control en curso pratico de php

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php
 
-$valorA = true;
-$valorB = false;
+$valorA = 5;
+$valorB = 2;
 
-$resultado = ($valorA and $valorB); // por precedencia de operadores el operador and lo que hace es tomar el primer valor igualarlo a resultado y despues hacer el operador logico ya que and va de izquierda a derecha y por ende toma primero el operador de igualddad, podemos evitar esto colocandole parentesis para ejecute primero el operador logico
-var_dump($resultado);
+$suma = $valorA + $valorB;
+var_dump($suma);
 
-$resultado = $valorA && $valorB; // a cambio los simbolos && toma primero el operador logico y despues el operador de igualdad
-var_dump($resultado);
+$resta = $valorA - $valorB;
+var_dump($resta);
 
-$resultado = ($valorA or $valorB); // sucede igual con or y ||, or sucede igual que and y || sucede igual que &&
-var_dump($resultado);
+$multiplicacion = $valorA * $valorB;
+var_dump($multiplicacion);
 
-$resultado = $valorA || $valorB; // sucede igual con or y ||, or sucede igual que and y || sucede igual que &&
-var_dump($resultado);
+$modulo = $valorA % $valorB;
+var_dump($modulo);
 
-$resultado = $valorA xor $valorB; // xor lo que hace es que si uno u otro es true el resultado es true sino sera false (esto quiere decir que ambos tiene que false para que de false), tambien sucede lo mismo que or y and
-var_dump($resultado);
+$division = $valorA / $valorB;
+var_dump($division);
 
-$resultado = !$valorA;
-var_dump($resultado);
+$potenciacion = $valorA ** $valorB;
+var_dump($potenciacion);
+
 ?>
 
