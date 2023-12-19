d72560abd3bc9cd8e8d2348e678f2930f668554f
Ecos1213 <diasflac1213@gmail.com>
2023-12-19 14:52:55 -0500

    Agrega clase de estructuras de control mas pratico y rapido del curso pratico de php

--- index.php
+++ index.php
@@ -1,25 +1,56 @@
 <?php
 
 $valorA = 5;
-$valorB = 2;
 
-$suma = $valorA + $valorB;
-var_dump($suma);
+if($valorA == 10) {
+    echo "Es igual a 10 \n";
+}else {
+    echo "No es igual a 10 \n";
+}
 
-$resta = $valorA - $valorB;
-var_dump($resta);
+$i = 0;
 
-$multiplicacion = $valorA * $valorB;
-var_dump($multiplicacion);
+while($i <= 10) { // por cada iteracion ejecutara el codico esto se le llama ciclo
+    echo $i++;
+}
 
-$modulo = $valorA % $valorB;
-var_dump($modulo);
+echo "\n";
 
-$division = $valorA / $valorB;
-var_dump($division);
+$i = 0;
+do { // primero ejecuta la linea de codigo y despues hace la iteracion de cada ciclo
+    echo $i++;
+}while($i <= 10);
 
-$potenciacion = $valorA ** $valorB;
-var_dump($potenciacion);
+echo "\n";
+
+$arr = array(1,2,3,4);
+for($i=0; $i<4; $i++) {
+    echo $arr[$i];
+}
+
+echo "\n";
+
+foreach($arr as $value) {
+    echo $value;
+}
+
+echo "\n";
+
+switch($valorA) {
+    case 1:
+        echo "El valor es 1";
+        break;
+    case 2:
+        echo "El valor es 2";
+        break;
+    case 3:
+        echo "El valor es 3";
+        break;
+    default:
+        echo "No es ninguno de los valores 1, 2 o 3";
+        break;
+}
+echo "\n";
 
 ?>
 
