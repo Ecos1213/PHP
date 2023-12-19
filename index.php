d2941ad3dd32acba55d1697096875cd17b6a62a4
Ecos1213 <diasflac1213@gmail.com>
2023-12-19 10:57:23 -0500

    Agrega clase de operador logico de curso pratico php y se entiende mas la precedencia de operadores especialmente tanto de and y or y un nuevo operador llamado xor que lo que hace es que si uno u otro es true el resultado es true

--- index.php
+++ index.php
@@ -1,26 +1,24 @@
 <?php
 
-    // si queremos interactuar php con html lo que podemos hacer es interactuar directamente escribiendo html en php, por esta razon en un inicio colocamos el tag de apertura como de final de php
-    // si queres usar html directamente recuerda que tienes que ejecutarlo de un servidor local asi como vimos en el curso anterior instalando xampp o usando linux en xampp deberia ejecutarse desde la carpeta htdocs en linux es var/www/html/index.php
-    // recuerda como estamos usando html estoy obligado a colocar el tag final de php
-    $zorritos = [
-        "https://randomfox.ca/images/1.jpg",
-        "https://randomfox.ca/images/2.jpg",
-        "https://randomfox.ca/images/3.jpg",
-    ];
+$valorA = true;
+$valorB = false;
+
+$resultado = ($valorA and $valorB); // por precedencia de operadores el operador and lo que hace es tomar el primer valor igualarlo a resultado y despues hacer el operador logico ya que and va de izquierda a derecha y por ende toma primero el operador de igualddad, podemos evitar esto colocandole parentesis para ejecute primero el operador logico
+var_dump($resultado);
+
+$resultado = $valorA && $valorB; // a cambio los simbolos && toma primero el operador logico y despues el operador de igualdad
+var_dump($resultado);
+
+$resultado = ($valorA or $valorB); // sucede igual con or y ||, or sucede igual que and y || sucede igual que &&
+var_dump($resultado);
+
+$resultado = $valorA || $valorB; // sucede igual con or y ||, or sucede igual que and y || sucede igual que &&
+var_dump($resultado);
+
+$resultado = $valorA xor $valorB; // xor lo que hace es que si uno u otro es true el resultado es true sino sera false (esto quiere decir que ambos tiene que false para que de false), tambien sucede lo mismo que or y and
+var_dump($resultado);
+
+$resultado = !$valorA;
+var_dump($resultado);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola! esto es HTML desde PHP!</h1>
-    <?php foreach($zorritos as $zorrito) { ?>
-        <img src="<?php echo $zorrito //aca imprimos las urls de las imagenes de los zorros y ten encuenta que y como vemos tambien podemos usar etiquetas de php de esta manera?>" >
-    <?php } ?>
-</body>
-</html>
