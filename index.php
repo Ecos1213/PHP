47f082ddaca7a98298ade8cfcc7438d2b0c8c2d7
Ecos1213 <diasflac1213@gmail.com>
2023-12-20 11:49:07 -0500

    Agrega dos archivos un html con un formulario que envia por metodo post a index.php y este script de php lo que hace es solo mostrar lo que viene conj la variable de serviro llamada $_REQUEST y mostramos sus datos mas humanamente posible con un print_r

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
-// aca estan todas las funciones matematicas que por defecto bienen en php https://www.php.net/manual/es/book.math.php
-echo max(9,4,3,2,6); // entrega el valor maximo se puede colocar varios parametros o colocar un array de numeros
-echo "\n";
-echo min(9,4,3,2,6); // entrega el valor minimo se puede colocar varios parametros o colocar un array de numeros
-echo "\n";
-echo pi(); // metodo que proporciona el numero de PI
-echo "\n";
-echo M_PI; // Constante que proporciona el numero de PI
-echo "\n";
-echo rand(); // el metodo rand nos permite darnos numeros aleatorios, este sirve para crear claves, para generar pruebas, para hacer alguna actividad 
-echo "\n";
-echo rand(1,9); // este metodo se puede colocar parametros o no, si lleva parametros tenemos que colocarle un minimo valor y como segundo parametro un maximo valor, donde comenzara desde el minimo hasta el maximo valor
-echo "\n";
-echo sqrt(25); // sirve para sacar la raiz cuadrada
-echo "\n";
-echo rand(40, 100);
+/**
+ * $_REQUEST es una variable global en PHP que se utiliza para recopilar datos de formularios HTML. 
+ * Puede recopilar datos de formularios enviados tanto con el método HTTP POST como con el método HTTP GET. 
+ * En otras palabras, $_REQUEST contiene datos de variables superglobales $_GET, $_POST y $_COOKIE.
+ * 
+ * Cuando se envía un formulario HTML con el método POST o GET, los datos del formulario se envían al script PHP del lado del servidor. 
+ * $_REQUEST se utiliza para recoger estos datos y hacerlos accesibles en el script PHP.
+ */
+    print_r($_REQUEST); // puedes acceder al valor del campo Nombre como un array osea asi: $_REQUEST["Nombre"]; y asi podemos tomar el valor que se envia desde el formulario html
+
+/**
+ * Es importante tener en cuenta que el uso de $_REQUEST puede ser menos seguro que acceder directamente a $_POST o $_GET según el contexto de la aplicación. 
+ * Esto se debe a que $_REQUEST incluye datos de cookies, y en algunas situaciones podría ser más seguro y específico utilizar 
+ * $_POST o $_GET según el método de envío del formulario. Además, el uso adecuado de funciones como htmlspecialchars o mysqli_real_escape_string
+ * es crucial para evitar problemas de seguridad como la inyección de código.
+ */
 ?>
 
