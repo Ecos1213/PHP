878cbcc101891214501a33dbe0c165c196cd10ec
Ecos1213 <diasflac1213@gmail.com>
2023-12-20 10:42:37 -0500

    Agrega curso de funciones matemticas predertimadas en el curso pratico de php

--- index.php
+++ index.php
@@ -1,31 +1,19 @@
 <?php
-
-$nombre = "Camila";
-$apellido = "Rojas";
-
-function concatenar($nombre, $apellido) {
-    echo "$nombre $apellido";
-}
-
-concatenar("Camila", "Rojas");
-
-function sumNumbers($numeros) {
-    $suma = 0;
-
-    for($i=0; $i<4; $i++) {
-        $suma += $numeros[$i];
-    }
-
-    echo "\nLa suma de los valores del array es: $suma\n";
-}
-
-$numeros = array(1,2,3,4);
-sumNumbers($numeros);
-
-$nombres = array("Ana", "Juan", "Camila");
-var_dump(is_array($nombres)); // is_array valida si la variable es un array, si es un array retornara un true y si no es un array retornara false
-
-
-
+// aca estan todas las funciones matematicas que por defecto bienen en php https://www.php.net/manual/es/book.math.php
+echo max(9,4,3,2,6); // entrega el valor maximo se puede colocar varios parametros o colocar un array de numeros
+echo "\n";
+echo min(9,4,3,2,6); // entrega el valor minimo se puede colocar varios parametros o colocar un array de numeros
+echo "\n";
+echo pi(); // metodo que proporciona el numero de PI
+echo "\n";
+echo M_PI; // Constante que proporciona el numero de PI
+echo "\n";
+echo rand(); // el metodo rand nos permite darnos numeros aleatorios, este sirve para crear claves, para generar pruebas, para hacer alguna actividad 
+echo "\n";
+echo rand(1,9); // este metodo se puede colocar parametros o no, si lleva parametros tenemos que colocarle un minimo valor y como segundo parametro un maximo valor, donde comenzara desde el minimo hasta el maximo valor
+echo "\n";
+echo sqrt(25); // sirve para sacar la raiz cuadrada
+echo "\n";
+echo rand(40, 100);
 ?>
 
