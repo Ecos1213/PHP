36aeb2d2b910a0b184dc516751233b0874e0c66f
Ecos1213 <diasflac1213@gmail.com>
2023-12-21 23:14:32 -0500

    Agrega clase de code review y buenas praticas

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * Desarrollar una aplicacion que muestra una lista de palabras que estan en desorden.
- * el usuario debe ordenar las letras para encontrar la palabra correcta e ingresarla.
- * despues de enviar sus resultados, el usuario podra ver si acerto o fallo y cual era la palabra correcta
+ * Como buena pratica todas los nombres de variable tienen que ser en ingles
+ * aunque colocamos hizimos el formulario en forma dinamica a travez de strings y con la ayuda de echo logramos que lo vieramos en el navegador, no es la forma ideal en el cual manejemos codigo html, el codigo html tiene su propia estructura y debe ser manejado a travez de un frontend con lo que tenga su estructura html su extension html y realizes esta conexion con lenguaje como javascript    
+ * manten una uniformidad en todo el documento esto implica que:
+ * si trabajas en un cierto idioma todo el documento tiene que estar en ese idioma
+ * tratar de llevar la declaracion de las variables en ingles
+ * 
  */
 
     $palabras = array("sol", "luna", "cielo", "luz", "estrellas", "lluvia");
